f18bac0ff6ac8fefbb52ad0bb506608c16cdaf61
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2020-10-02 00:38:24 +0700

    Tried heart

--- NameServer.py
+++ NameServer.py
@@ -6,17 +6,21 @@
 
 # Each file should be replicated on at least 2 Storage Servers. If one of the Storage Server goes down, files, that is stored should be replicated to another Storage Server.
 
+# Choose the serv with the least load
+
 import socket
 from threading import Thread
 import os
 import re
+from time import sleep
 
-servers = []
+
+SONS = []
 BUFF = 1488 # Unified constant
-
+PORT = 1488
 
 # Thread to listen one particular client
-class ClientListener(Thread):
+class HeartListener(Thread):
     def __init__(self, name: str, sock: socket.socket):
         super().__init__(daemon=True)
         self.sock = sock
@@ -24,39 +28,43 @@ class ClientListener(Thread):
 
     # clean up
     def _close(self):
-        servers.remove(self.sock)
+        SONS.remove(self.sock)
         self.sock.close()
-        print(self.name + ' disconnected')
+        print(self.name + ' dead')
 
     def run(self):
 
         # Unpack the metadata that goes as the first package
-        received = self.sock.recv(BUFF).decode()
-        filename, filesize = received.split("?CON?")
-
-        print(f"[{self.name}] Starting transfer of {filename}")
-
-        # Make the data actually useful
-        filename = os.path.basename(filename)
-        filesize = int(filesize)
-
-        # Counter initialization
-        sas = 0.0
+        # received = self.sock.recv(BUFF).decode()
+        # filename, filesize = received.split("?CON?")
+        #
+        # print(f"[{self.name}] Starting transfer of {filename}")
+        #
+        # # Make the data actually useful
+        # filename = os.path.basename(filename)
+        # filesize = int(filesize)
+        #
+        # # Counter initialization
+        # sas = 0.0
 
         # Receive/Write
 
-        with open(filename, "wb") as f:
+        while self.sock.recv(BUFF):
+            print(f' {self.name} IS ALIVE!!!!!!! ')
+            sleep(2)
 
-            for i in range(filesize):
-                # Will return zero when done
-                rcv = self.sock.recv(BUFF)
-                if rcv:
-                    # Write received data
-                    sas += BUFF
-                    f.write(rcv)
-                else:
-                    print(f"[{self.name}] Transfer complete!!")
-                    break
+        # with open(filename, "wb") as f:
+        #
+        #     for i in range(filesize):
+        #         # Will return zero when done
+        #         rcv = self.sock.recv(BUFF)
+        #         if rcv:
+        #             # Write received data
+        #             sas += BUFF
+        #             f.write(rcv)
+        #         else:
+        #             print(f"[{self.name}] Transfer complete!!")
+        #             break
         self._close()
 
 
@@ -69,18 +77,18 @@ def main():
     # so if we close and imidiatly start server again – we'll get error
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # listen to all interfaces at 8800 port
-    sock.bind(('', 1488))
+    sock.bind(('', PORT))
     sock.listen()
-    print("Waiting for clients...")
+    print("Waiting for SONS...")
     while True:
         # blocking call, waiting for new client to connect
         con, addr = sock.accept()
-        servers.append(con)
-        name = 'u' + str(next_name)
+        SONS.append(con)
+        name = 'SON ' + str(next_name)
         next_name += 1
-        print(f"[{name}] " + str(addr) + ' connected')
+        print(f"{name} " + str(addr) + ' WAS FOUND!!!')
         # start new thread to deal with client
-        ClientListener(name, con).start()
+        HeartListener(name, con).start()
 
 
 if __name__ == "__main__":
